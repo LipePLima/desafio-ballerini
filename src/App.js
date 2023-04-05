8ed8c425456d33e86deffe1598ef658f7b359e31
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-04-05 18:37:36 -0300

    style: Estilo do input finalizado e imagem principal colocada

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ function App() {
   return (
     <div className="App">
       <section className="App-section">
+        <div className='section-image'></div>
         <h1 className="section-title">Faça algo mágico...</h1>
         <p className="section-text">Ache sempre tudo em um só lugar</p>
-        <input className="section-input" placeholder='Faça algo mágico...'></input>
+        <div className='section-container___input'>
+          <span class="material-symbols-outlined container___input-icon">search</span>
+          <input className="container___input-input" placeholder='Faça algo mágico...'></input>
+        </div>
+        
       </section>
     </div>
   );

c53d447476cc9382bf0032f48e4411693882e1c8
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-04-04 21:52:48 -0300

    feat: Título, texto e input feito

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <section className="App-section">
+        <h1 className="section-title">Faça algo mágico...</h1>
+        <p className="section-text">Ache sempre tudo em um só lugar</p>
+        <input className="section-input" placeholder='Faça algo mágico...'></input>
+      </section>
     </div>
   );
 }
